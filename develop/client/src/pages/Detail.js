c38367eff135f7162f1c8da79200d3aeeffb5c33
bjennings90 <101131665+bjennings90@users.noreply.github.com>
2022-10-18 17:05:18 -0500

    Implement Cart Toggle

--- develop/client/src/pages/Detail.js
+++ develop/client/src/pages/Detail.js
@@ -6,6 +6,9 @@ import { useStoreContext } from '../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../utils/actions';
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
+import Cart from '../components/Cart';
+import ProductList from "../components/ProductList";
+import CategoryMenu from "../components/CategoryMenu";
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
@@ -51,6 +54,7 @@ function Detail() {
         </div>
       ) : null}
       {loading ? <img src={spinner} alt="loading" /> : null}
+        <Cart />
     </>
   );
 }
